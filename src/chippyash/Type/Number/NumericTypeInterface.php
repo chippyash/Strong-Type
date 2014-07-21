84acf53b90fa1f020a4ee54dde0071ddbf5a1ba4
Ashley Kitson <ashley@zf4.biz>
2014-07-21 06:41:22 +0100

    add numeric type interface

---
+++ src/chippyash/Type/Number/NumericTypeInterface.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Hard type support
+ * For when you absolutely want to know what you are getting
+ *
+ * @author Ashley Kitson <akitson@zf4.biz>
+ * @copyright Ashley Kitson, UK, 2012
+ * @licence GPL V3 or later : http://www.gnu.org/licenses/gpl.html
+ */
+namespace chippyash\Type\Number;
+
+/**
+ * A dummy interface to mark numeric types
+ */
+interface NumericTypeInterface {}
