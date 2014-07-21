e802dd6babc04d330f86c3144e9c85cc873eafb4
Ashley Kitson <ashley@zf4.biz>
2014-07-21 09:06:15 +0100

    add numeric type interfaces

--- src/chippyash/Type/Number/FloatType.php
+++ src/chippyash/Type/Number/FloatType.php
@@ -11,11 +11,12 @@
 namespace chippyash\Type\Number;
 
 use \chippyash\Type\AbstractType;
+use chippyash\Type\Number\NumericTypeInterface;
 
 /**
  * Floating number Type
  */
-class FloatType extends AbstractType
+class FloatType extends AbstractType implements NumericTypeInterface
 {
 
     protected function typeOf($value)
