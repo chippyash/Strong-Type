d170ddc90d0454c6432b97d2e6e897884cf54ea4
Ashley Kitson <ashley.kitson@first-utility.com>
2014-07-24 08:02:55 +0100

    rebase natural and whole ints onto int type

--- test/src/chippyash/Type/String/DigitTypeTest.php
+++ test/src/chippyash/Type/String/DigitTypeTest.php
@@ -24,6 +24,15 @@ class DigitTypeTest extends \PHPUnit_Framework_TestCase
         $t = new DigitType('123.45');
         $this->assertInternalType('string', $t->get());
         $this->assertEquals('12345', $t->get());
+
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     * @expectedExceptionMessage Object of class stdClass could not be converted to string
+     */
+    public function testConvertNonStringableTypeThrowsException()
+    {
+        $t = new DigitType(new \stdClass());
+    }
 }
