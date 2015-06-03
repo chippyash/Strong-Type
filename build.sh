fc552bb9c16241285f9caea6fa39305e535ebe9d
Ashley Kitson <ashley@zf4.biz>
2015-06-03 23:37:05 +0100

    update docs after massive refactor

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../source/Strong-Type/vendor/bin/apigen --title "Chippyash Strong Types" --php no --source-code yes --source ../../source/Strong-Type/src/chippyash/Type/ --destination .
+apigen --title "Chippyash Strong Types" --php no --source-code yes --source ../../source/Strong-Type/src/chippyash/Type/ --destination .
